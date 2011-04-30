248cf5934f1d23a29f3ed672c76f972c01037b66
John Connuck <john_connuck@brown.edu>
2011-04-30 12:01:08 -0400

    added all files from my model directory. I realized some were not tracked by git

---
+++ src/jvm/final_project/model/FencerSeed.java
@@ -0,0 +1,55 @@
+package final_project.model;
+
+import java.util.*;
+
+public class FencerSeed extends PlayerSeed {
+	FencerRoundResults _fencerResults;
+
+	public FencerSeed(PlayerRoundResults playerResults) {
+		super(playerResults);
+		_fencerResults = (FencerRoundResults)playerResults;
+	}
+
+	@Override
+	boolean isClearWinner(PlayerSeed playerOpponent) {
+		if (this.isOfSameClass(playerOpponent)) {
+			FencerRoundResults opponentResults = (FencerRoundResults)playerOpponent.getPlayerResults();
+			if(this.hasBetterWinPercentage(opponentResults) || 
+				hasBetterTouchSpread(opponentResults) ||
+				hasMoreTouches(opponentResults))
+				return true;
+			return false;
+		}
+		else 
+			throw new ClassCastException();
+	}
+
+	private boolean isOfSameClass(PlayerSeed playerOpponent) {
+		return _fencerResults.getClass() == playerOpponent.getPlayerResults().getClass();
+	}
+	
+	private boolean hasBetterWinPercentage(FencerRoundResults opponentResults) {
+		return _fencerResults.getWinningPercentage() > opponentResults.getWinningPercentage();
+	}
+	
+	private boolean hasBetterTouchSpread(FencerRoundResults opponentResults) {
+		return _fencerResults.getTouchSpread() > opponentResults.getTouchSpread();
+	}
+	
+	private boolean hasMoreTouches(FencerRoundResults opponentResults) {
+		return _fencerResults.getTouchesScored() > opponentResults.getTouchesScored();
+	}
+	
+	@Override
+	boolean isTiebreakWinner(PlayerSeed opponent) {
+		return chooseRandomWinner();
+	}
+	
+	private boolean chooseRandomWinner() {
+		Random r = new Random();
+		int rand = r.nextInt(2);
+		if (rand == 0)
+			return true;
+		return false;
+	}
+}
---
+++ src/jvm/final_project/model/PlayerResult.java
@@ -0,0 +1,19 @@
+package final_project.model;
+
+public class PlayerResult {
+	private int _playerId;
+	private int _playerScore;
+
+	public PlayerResult(int playerId, int playerScore) {
+		_playerId = playerId;
+		_playerScore = playerScore;
+	}
+
+	public int getPlayerId() {
+		return _playerId;
+	}
+	
+	public int getPlayerScore() {
+		return _playerScore;
+	}
+}
