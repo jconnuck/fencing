478028c94a4c831bb8b6c91c5958bc92c50c6667
John Connuck <john_connuck@brown.edu>
2011-04-30 16:05:19 -0400

    renamed Result.java to CompleteResult.java and created superclass Result.java

--- src/jvm/final_project/model/Result.java
+++ src/jvm/final_project/model/Result.java
@@ -1,42 +1,17 @@
 package final_project.model;
 
 public class Result {
-	private int _ref;
-	private int _pointsToWin;
-	private PlayerResult _winnerResult, _loserResult;
+	protected int _player1;
+	protected int _player2;
 
-	public Result(PlayerResult winnerResult, PlayerResult loserResult, int ref) {
-		_winnerResult = winnerResult;
-		_loserResult = loserResult;
-		_ref = ref;
+	public int[] getPlayers() {
+		return new int[] {_player1, _player2};
 	}
 
-	//Where is this used?
-	public void setPointsToWin(int toWin) {
-		_pointsToWin = toWin;
+	public int getPlayer1(){
+		return _player1;
 	}
-	
-	public int getPointsToWin() {
-		return _pointsToWin;
-	}
-
-	public int getWinner() {
-		return _winnerResult.getPlayerId();
-	}
-
-	public int getLoser() {
-		return _loserResult.getPlayerId();
-	}
-
-	public int getRef(){
-		return _ref;
-	}
-
-	public int getWinnerScore(){
-		return _winnerResult.getPlayerScore();
-	}
-
-	public int getLoserScore(){
-		return _loserResult.getPlayerScore();
+	public int getPlayer2(){
+		return _player2;
 	}
 }
