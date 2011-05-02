d53c9a2ec799bc88e777d00b3962427329c7aad1
jonleavitt <jon.s.leavitt@gmail.com>
2011-05-01 23:35:01 -0400

    making progress on DERound.java, working on populateBracket()

--- src/jvm/final_project/model/DERound.java
+++ src/jvm/final_project/model/DERound.java
@@ -9,7 +9,7 @@ public class DERound implements IRound {
 	private int _bracketSize;    /*!< The bracket size (How many slots there are in the first round of DEs) (must be a power of 2) */
 	private int _currentBracket; /*!< The number of slots in the current stage of the DE round (must be a power of 2 and <= _bracketSize*/
 	private static int POINTS_TO_WIN;
-	
+
 	/**
 	 * Sets up the DE round by cutting the bottom _cut percentage of the competitors and filling the bracket with the remaining competitors.
 	 */
@@ -18,7 +18,7 @@ public class DERound implements IRound {
 		calcBracketSize();
 		populateBracket();
 	}
-	
+
 	/**
 	 * Cuts the bottom _cut percentage of fencers from _seeding
 	 */
@@ -26,13 +26,13 @@ public class DERound implements IRound {
 		int newEnd = (int) Math.ceil(_seeding.size() * (1 - _cut));
 		_seeding = _seeding.subList(0, newEnd);
 	}
-	
+
 	/**
 	 * Calculates the proper bracket size using the number of fencers in the round
 	 */
 	private void calcBracketSize() throws IllegalArgumentException{
-		if(_seeding.size() < 2) 
-			throw new IllegalArgumentException("Attempted to build a bracket for less than 2 competitors");
+		if(_seeding.size() < 2)
+			throw new IllegalArgumentException("Attempted to build a bracket for less than 2 competitors.");
 		int curSize = 2;
 		int totalSize = 2;
 		while(curSize < _seeding.size()) {
@@ -42,11 +42,26 @@ public class DERound implements IRound {
 		_bracketSize = curSize;
 		_matches = new Result[totalSize];
 	}
-	
-	public void populateBracket(){
-		populateBracketHelper(0, 2, 1);		
+
+	public void populateBracket() throws IllegalStateException{
+		if(_matches == null){
+			throw new IllegalStateException("_matches not yet instantiated.");
+		}
+		populateBracketHelper(0, 2, 1);
+		//for(int i = 0; )
+		for(int i = 0; i < _matches.length - _bracketSize / 2 - 1; i++){
+			_matches[i] = null;
+		}
 	}
-	
+
+	/**
+	 *Helper function for populateBracket() that takes the values that represent the seeding of the competitors
+	 *in the current IncompleteResults and replaces them with the int id of the actual competitor of that seed.
+	 */
+	public void switchSeedsForCompetitors(){
+
+	}
+
 	private void populateBracketHelper(int index, int currentBracketSize, int currentSeed){
 		if(index < 0)
 			throw new IllegalArgumentException("Index cannot be negative.");
@@ -55,26 +70,26 @@ public class DERound implements IRound {
 		_matches[index] = new IncompleteResult(	currentSeed,
 												currentBracketSize - currentSeed + 1,
 												POINTS_TO_WIN);
-		populateBracketHelper(	2 * index + 1, 
-								currentBracketSize * 2, 
+		populateBracketHelper(	2 * index + 1,
+								currentBracketSize * 2,
 								currentSeed);
-		populateBracketHelper(	2 * index + 2, 
-								currentBracketSize * 2, 
+		populateBracketHelper(	2 * index + 2,
+								currentBracketSize * 2,
 								currentBracketSize - currentSeed + 1);
 	}
-	
+
 	public void setCut(double newCut){
 		_cut = newCut;
 	}
-	
+
 	public double getCut() {
 		return _cut;
 	}
-	
+
 	public int getCurrentBracketSize() {
 		return _currentBracket;
 	}
-	
+
 	public IncompleteResult getNextBout() throws NoSuchBoutException{
 		for(Result result : _matches) {
 			if(result instanceof IncompleteResult) {
@@ -83,7 +98,7 @@ public class DERound implements IRound {
 		}
 		throw new NoSuchBoutException("No such bout exists in this DERound");
 	}
-	
+
 	public void addCompleteResult(CompleteResult newResult) throws NoSuchBoutException {
 		Result tempResult;
 		for(int i = 0; i < _bracketSize /2; i++) {
@@ -97,7 +112,6 @@ public class DERound implements IRound {
 					throw new NoSuchBoutException("This bout has already been completed");
 				}
 			}
-			
 		}
 	}
 
@@ -113,9 +127,4 @@ public class DERound implements IRound {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
-	
-
-
-
 }
