6ba60589b554730459280e209b80516397b77e30
John Connuck <john_connuck@brown.edu>
2011-05-04 21:51:34 -0400

    added library folder

---
+++ lib/deps.clj
@@ -0,0 +1,1 @@
+{:dependencies {clojure {:version "1.2.0"}, clojure-contrib {:version "1.2.0"}, junit {:version "4.8.2"}}, :dev-dependencies {swank-clojure {:version "1.2.1"}}, :ext-dependencies {}}
