d3bfc4daef3c737ace45bb5830691b018ff4717b
jonleavitt <jon.s.leavitt@gmail.com>
2011-05-06 02:55:03 -0400

    augmenting TournamentController

--- src/jvm/final_project/control/TournamentController.java
+++ src/jvm/final_project/control/TournamentController.java
@@ -1,6 +1,6 @@
 package final_project.control;
 
-import java.util.Collection;
+import java.util.*;
 
 import final_project.model.*;
 import final_project.model.DERound.NoSuchMatchException;
@@ -8,9 +8,11 @@ import final_project.model.DERound.NoSuchMatchException;
 public class TournamentController {
 
 	private Collection<EventController> _events;
+	private IDataStore _dataStore;
 
 	public TournamentController() {
-
+		_events = new LinkedList<EventController>();
+//		_dataStore = new DataStore();
 	}
 
 	/**
@@ -27,7 +29,7 @@ public class TournamentController {
 	 * @param bout A completed bout to be added to results
 	 * @param stripID The number of the strip the bout was fenced on
 	 */
-	public void addResult(CompleteResult result, int ref) {
+	public void addCompletedResult(CompleteResult result, int ref) {
 		for(EventController event : _events){
 			if(event.hasRef(ref)){
 				try {
