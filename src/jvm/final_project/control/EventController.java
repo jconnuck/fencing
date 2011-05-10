b196c2a64f24456d982e3dd7c2ecda86957719e7
Josh <joshua_grill@brown.edu>
2011-05-10 02:01:02 -0400

    Fixed startDERound (constructor for DERound)

--- src/jvm/final_project/control/EventController.java
+++ src/jvm/final_project/control/EventController.java
@@ -89,7 +89,8 @@ public class EventController {
 	public boolean startDERound(double cut){
 		if(_state != State.POOLS)
 			return false;
-		//_deController = new DERoundController(_dataStore, )
+		convertPlayersListToSortedSeeding();
+		_deController = new DERoundController(_dataStore, _stripController, _players, cut);
 		return true;
 	}
 
