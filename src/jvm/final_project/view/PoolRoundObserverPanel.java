2de0759a54108b52dcc5838ee6ae9ccb89d41079
John Connuck <john_connuck@brown.edu>
2011-05-10 04:01:23 -0400

    changed background of PoolRoundObserverPanel to black

--- src/jvm/final_project/view/PoolRoundObserverPanel.java
+++ src/jvm/final_project/view/PoolRoundObserverPanel.java
@@ -4,6 +4,7 @@ import javax.swing.JPanel;
 import final_project.control.TournamentController;
 import final_project.model.store.IDataStore;
 import final_project.model.*;
+import java.awt.Color;
 
 public class PoolRoundObserverPanel extends JPanel {
 
@@ -11,6 +12,7 @@ public class PoolRoundObserverPanel extends JPanel {
 	 * Create the panel.
 	 */
 	public PoolRoundObserverPanel(TournamentController tournament) {
+		setBackground(Color.BLACK);
 		//TODO make a for loop, making anew PoolObserverPanel for each pool and add it
         int i=1;
         for (Pool p : tournament.getPools(0)) {
