2ae53569014f50f85712c1ca6c692e730f17946a
jonleavitt <jon.s.leavitt@gmail.com>
2011-05-11 14:16:09 -0400

    assignReferees actually returns a correct value now

--- src/jvm/final_project/control/DataFormattingHelper.java
+++ src/jvm/final_project/control/DataFormattingHelper.java
@@ -31,8 +31,10 @@ public class DataFormattingHelper implements Constants {
 		Object[][] toReturn = new Object[numPeople][NUM_COLS_SIGN_IN];
 
 		//Making one blank row so that the GUI does not break on empty input
-		for(int i=0; i < NUM_COLS_SIGN_IN; i++)
+		for(int i=0; i < NUM_COLS_SIGN_IN; i++){
+			System.out.println(toReturn == null);
 			toReturn[0][i] = "";
+		}
 
 		int index = 0;
 		for (IPerson i: _dataStore.getPeople()) {
@@ -107,7 +109,7 @@ public class DataFormattingHelper implements Constants {
 				int id = ((Integer)(i.getWatched().iterator().next())).intValue();
 				System.out.println("ID in data formatting helper subscribe panel" + id); //TODO println
 				IObservable followed = _dataStore.getObservable(id);
-				if(followed instanceof IClub) 
+				if(followed instanceof IClub)
 					toReturn[index][2] =  ((IClub) followed).getName() + "";
 			}
 
@@ -136,23 +138,23 @@ public class DataFormattingHelper implements Constants {
 
 	public Object[][] getPoolRefListTable(Pool pool) {
 		Object[][] toReturn = new Object[pool.getPlayers().size()][NUM_COLS_POOL_REF_LIST];
-		
+
 		int i = 0;
 		for(int p: pool.getPlayers()) {
 			//Not checking for data store null ptrs but whatever.
 			toReturn[i][0] = _dataStore.getPerson(p).getFirstName() + " " + _dataStore.getPerson(p).getLastName();
-			
+
 			toReturn[i][1] = "";
 			Iterator<Integer> clubs = _dataStore.getPlayer(p).getClubs().iterator();
 			if(clubs.hasNext()) {
 				toReturn[i][1] = _dataStore.getClub(clubs.next()).getName();
 			}
-			
+
 			toReturn[i][2] = i+1;
-			
+
 			i++;
 		}
-		
+
 		return toReturn;
 	}
 
