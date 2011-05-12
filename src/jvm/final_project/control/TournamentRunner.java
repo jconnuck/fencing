30b82a8efe39a52ef199114ad5d0ce15c56a1a1b
jonleavitt <jon.s.leavitt@gmail.com>
2011-05-12 03:43:18 -0400

    disabled TournamentRunner

--- src/jvm/final_project/control/TournamentRunner.java
+++ src/jvm/final_project/control/TournamentRunner.java
@@ -9,13 +9,13 @@ public class TournamentRunner implements Runnable {
 	@Override
 	public void run() {
 		//public void returnResults(int refID, int winnerID, int winnerScore, int loserID, int loserScore) {
-		_smsController.returnResults(0, 1, 5, 2, 2);
+	/*	_smsController.returnResults(0, 1, 5, 2, 2);
 		System.out.println("running runner");
 		_smsController.returnResults(0, 0, 5, 3, 2);
 		_smsController.returnResults(0, 1, 5, 0, 2);
 		_smsController.returnResults(0, 2, 5, 3, 2);
 		_smsController.returnResults(0, 0, 5, 2, 2);
-		_smsController.returnResults(0, 1, 5, 3, 2);
+		_smsController.returnResults(0, 1, 5, 3, 2);*/
 	}
 
 }
